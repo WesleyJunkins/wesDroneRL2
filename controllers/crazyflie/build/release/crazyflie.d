9ef60a9eeea28604edcc9b150784c6c728d5e47c
Wesley Junkins <wesleyjunkins@gmail.com>
2025-10-07 17:04:24 -0500

    new version works

---
+++ controllers/crazyflie/build/release/crazyflie.d
@@ -0,0 +1,13 @@
+build/release/crazyflie.o: crazyflie.c \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera_recognition_object.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/distance_sensor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/gps.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/gyro.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/inertial_unit.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/keyboard.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h \
+  pid_controller.h
